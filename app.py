851bee3cadf4a6ee62f7cd7c881d80632b104624
JustLann <arivinsantos6@gmail.com>
2024-09-10 20:22:41 +0700

    Contoh Update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-def luas():
+def d():
     t = int(input("Masukkan Tinggi : "))
     a = int(input("Masukkan Alas : "))
     luas = t * a 
     print("Luas Segitiga Adalah :", luas)
-luas()
+d()
+
+def datar():
+    k = int(input("Masukkan Tinggi : "))
+    s = int(input("Masukkan Alas : "))
+    datars = k * s 
+    print("datar Segitiga Adalah :", datars)
+datar()
+
